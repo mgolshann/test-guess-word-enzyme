91ee7652c152bd11ee49fb48a9f95c604548bcd0
mgolshann <m_golshan85@yahoo.com>
2021-06-16 00:21:31 -0700

    test Congrats props

--- src/components/congrats/__test__/congrats.test.js
+++ src/components/congrats/__test__/congrats.test.js
@@ -7,8 +7,10 @@ import checkPropTypes from 'check-prop-types';
 
 Enzyme.configure({ adapter: new Adapter() })
 
+
 test("renders without error", () => {
-    const wrapper = shallow(<Congrats />);
+    const props = { success: true };
+    const wrapper = shallow(<Congrats {...props} />);
     const component = wrapper.find('[data-test="component-congrats"]');
     expect(component.length).toBe(1);
 });
@@ -27,3 +29,11 @@ test("renders message congratulation when `success` prop is false", () => {
     const message = wrapper.find('[data-test="congrats-message"]');
     expect(message.text()).toBe('');
 });
+
+
+test("I want to check prop types and prevent error", () => {
+    const expectedProps = { success: true };
+    const propError = checkPropTypes(Congrats.propTypes, expectedProps, 'prop', Congrats.name)
+    //if props is true the error it return is undefined so :
+    expect(propError).toBeUndefined();
+})
